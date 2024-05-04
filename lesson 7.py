c5501fef08f5da8d7ddeaf90c9c6178e3817f49e
Timur Tursunov <t.timon2003@gmail.com>
2024-05-04 18:50:14 +0300

    qwerty

---
+++ lesson 7.py
@@ -0,0 +1,39 @@
+#1
+
+# a = 5
+# if a > 10:
+#     print("good job")
+# else:
+#     print("lox")
+
+#2
+
+# a = 11
+# if a > 10 and a < 20:
+#     print(True)
+# else:
+#     print(False)
+
+#3
+
+# a = -5
+# if a < 0 and a > -10:
+#     print(True)
+# else:
+#     print(False)
+# if a > -10 and a > -8:
+#     print(True)
+# else:
+#     print(False)
+# if a > -8 and a < 30 :
+#     print(True)
+# else:
+#     print(False)
+
+#4
+
+# a = ['a', 'b','c','d','e']
+# if len(a) < 6:
+#     print("True")
+# elif len(a) > 0:
+#     print("False")
